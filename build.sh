55236d8070c7a7b81947dd2eab9aae44eee984cf
Yuan Li <liyuan989@gmail.com>
2015-02-03 12:29:35 +0800

    Modify build.sh and CMakelists.txt for Documented flags

--- build.sh
+++ build.sh
@@ -8,9 +8,9 @@ BUILD_BIR=${CURRENT_DIR}/build/${BUILD_TYPE}
 BUILD_NO_EXAMPLES=${BUILD_NO_EXAMPLES:-0}
 BUILD_NO_TEST=${BUILD_NO_TEST:-0}
 
-if [ ${BUILD_TYPE} != "release" ] && [ ${BUILD_TYPE} != "debug" ]
+if [ ${BUILD_TYPE} != "Release" ] && [ ${BUILD_TYPE} != "Debug" ]
 then
-    echo "Usage: BUILD_TYPE=debug/release"
+    echo "Usage: BUILD_TYPE=Debug/Release"
 else
     mkdir -p ${BUILD_BIR}
     cd ${BUILD_BIR}
