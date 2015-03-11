2acd6d0ae0288829e71c4f0ed67fd9d439802d9d
Yuan Li <liyuan989@gmail.com>
2015-03-11 09:17:07 +0800

    Add disconnected

--- src/TcpConnection.h
+++ src/TcpConnection.h
@@ -87,6 +87,11 @@ public:
         return state_ == kConnected;
     }
 
+    bool disconnected() const
+    {
+        return state_ == kDisconnected;
+    }
+
     void setContext(const boost::any& context)
     {
         context_ = context;
