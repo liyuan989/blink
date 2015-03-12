3d5d5a854ebdb0c52405b2c35ad207faf3e322f0
Yuan Li <liyuan989@gmail.com>
2015-03-12 11:20:23 +0800

    Add down_cast support for shared_ptr

--- src/Callbacks.h
+++ src/Callbacks.h
@@ -9,6 +9,22 @@
 namespace blink
 {
 
+// Adapted from google-protobuf stubs/common.h
+template<typename To, typename From>
+inline boost::shared_ptr<To> down_pointer_cast(const boost::shared_ptr<From>& f)
+{
+    if (false)
+    {
+        implicit_cast<From, To>(0);
+    }
+
+#ifndef NDEBUG
+    assert(f == NULL || dynamic_cast<To*>(boost::get_pointer(f)) != NULL);
+#endif
+
+    return boost::static_pointer_cast<To>(f);
+}
+
 class Buffer;
 class TcpConnection;
 
